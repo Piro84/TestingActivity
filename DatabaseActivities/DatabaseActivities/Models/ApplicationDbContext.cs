6c7e54ecfda434f1f7d4d8e089f43702dfc86a40
Unknown <19vign@elmbrookstudents.org>
2019-06-07 00:10:17 -0500

    ehhhh shit this gey

---
+++ DatabaseActivities/DatabaseActivities/Models/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using DatabaseActivities.Models.Entity;
+
+namespace DatabaseActivities.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ DatabaseActivities/DatabaseActivities/Models/Entity/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseActivities.Models.Entity
+{
+    public class Student:IEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public DateTime birthdate { get; set; }
+        public double gpa { get; set; }
+
+    }
+}
